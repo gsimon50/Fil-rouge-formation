7b71802b2c1d7d2fe9738e8a5a56f81a55a2f4ad
Geoffrey <geoffrey.simon@monkey-monk.fr>
2025-05-02 14:15:18 +0200

    Suite

--- front/src/app/page/homepage/home/home.component.ts
+++ front/src/app/page/homepage/home/home.component.ts
@@ -4,8 +4,6 @@ import { TopStoriesComponent } from "../top-stories/top-stories.component";
 import { TopVideoComponent } from "../top-video/top-video.component";
 import { ArticleComponent } from "../article/article.component";
 import { MostPopularComponent } from "../most-popular/most-popular.component";
-import { MenuComponent } from "../../menu/menu.component";
-
 @Component({
   selector: 'app-home',
   imports: [
@@ -14,7 +12,6 @@ import { MenuComponent } from "../../menu/menu.component";
     TopVideoComponent, 
     ArticleComponent, 
     MostPopularComponent,
-    MenuComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
