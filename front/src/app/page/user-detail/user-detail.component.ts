ef1e252d23f6cd8a823617c6d701d1677df3bf73
Geoffrey <geoffrey.simon@monkey-monk.fr>
2025-05-05 23:18:18 +0200

    On avance - Gestion du login

--- front/src/app/page/user-detail/user-detail.component.ts
+++ front/src/app/page/user-detail/user-detail.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+
+  constructor(private http: HttpClient) {}
+  email: string = "";
+  password: string = "";
+  isLoggedIn: boolean = false;
+
+
+
+  ngOnInit(): void {
+    // Charger l'état de connexion depuis le localStorage
+    const storedLoginState = localStorage.getItem('isLoggedIn');
+    this.isLoggedIn = storedLoginState === 'true';
+    this.email = localStorage.getItem('userName') || '';
+  }
+
+  onLogin(): void {
+    if (this.email && this.password) {
+      this.http.post('https://your-api-endpoint.com/login', {
+        email: this.email,
+        password: this.password
+        
+      }).subscribe({
+        next : (response) => {
+          this.isLoggedIn = true;
+          localStorage.setItem('isLoggedIn', 'true');
+          localStorage.setItem('userName', this.email);        
+        },
+        error : (error : any) => {
+          alert('Invalid credentials');
+        },
+        complete : () => {
+          console.log('Requete ApiArticle terminée');
+        }
+      });
+    }
+  }
+
+  onLogout(): void {
+    this.isLoggedIn = false;
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('userName');
+  }
+
 
 }
